09c1892f54eb59ea0fbfafa4f30d587bfdf77546
itgsod-Erico-Pizarro-Gutierrez <erico.pizarrogutierrez@elev.ga.it-gymnasiet.se>
2017-10-25 13:18:11 +0200

    sl.ticket

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,18 @@
 def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+    print age, student,senior
+    if age <=18:
+        return 'halv'
+    elif age >=64:
+        return 'halv'
+    else:
+        return 'hel'
 
 
 
 
 
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+
+print 'barn 11',fares(11)
+print 'pension 63',fares(63)
+print 'student 25', fares(25,student=True)
+print 'vuxen 45',fares(45)
